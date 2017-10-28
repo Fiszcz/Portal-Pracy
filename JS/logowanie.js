836dba584eac7996b05c6a8553f2074afb0d1835
Fiszcz <fiszczu@gmail.com>
2017-10-28 23:48:35 +0200

    Revert "Dodanie dokumentacji."
    This reverts commit a9d7bad9e9f3f4ef5021fbc1475dd893f55fbc5d.

--- JS/logowanie.js
+++ JS/logowanie.js
@@ -18,38 +18,8 @@ app.controller('myCtrl', function($scope,$http) {
         });
     };
     $scope.zarejestruj = function () {
-        $scope.bladHasla = false;
-        $scope.bladLoginu = false;
-        if($scope.login+''.length < 5) {
-            $scope.bladLoginu = true;
-            $scope.trescBladLoginu = "Podałeś zbyt krótki login";
-        }
-        if($scope.haslo1+''.length < 7 ){
-            $scope.bladHasla = true;
-            $scope.trescBladHasla = "Zbyt krótkie hasło!";
-            return;
-        }
         if($scope.haslo1 != $scope.haslo2){
-            $scope.bladHasla = true;
-            $scope.trescBladHasla = "Podane hasła nie są takie same!";
-            return;
+
         }
-        if($scope.bladLoginu == true)
-            return;
-        $http({
-            method : "POST",
-            url : "gooddfsfs.com"
-        }).then(function sukces(response) {
-            if(response == 2){
-                $scope.bladLoginu = 1;
-                $scope.trescBladLoginu = "Taki login już istnieje. Wybierz inny!";
-            }
-            else if(response == 3){
-                $scope.bladEmail = 1;
-                $scope.trescBladEmail = "Taki adres e-mail już widnieje w naszej bazie danych";
-            }
-            else {}
-            //przekierowanie
-        });
     }
 });
