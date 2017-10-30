8b48739deb3b91315da90dd850252fb1e1fedbeb
Fiszcz <fiszczu@gmail.com>
2017-10-30 23:40:24 +0100

    Stworzenie skryptów do Strony Głównej. I przepisanie strony html dla edycji profilu pod AngularJS.

--- JS/logowanie.js
+++ JS/logowanie.js
@@ -1,41 +1,82 @@
 var app = angular.module('glowna', []);
 app.controller('myCtrl', function($scope,$http) {
     $scope.czerwony = false;
+
+    //Do Logowania
     $scope.wyslij = function () {
+        if($scope.login === undefined || $scope.haslo === undefined){
+            $scope.czerwony = 4;
+            $scope.tekstBledu = "Wprowadz dane do logowania!";
+            return;
+        }
+        var logowanieW = {login: $scope.login, haslo: $scope.haslo};
+        logowanieW = JSON.stringify(logowanieW);
         $http({
             method : "POST",
             url : "gooddfsfs.com"
         }).then(function sukces(response) {
+            if(response==2){
+                $scope.czerwony = 4;
+                $scope.tekstBledu = "Błędne dane do logowania!";
+            }
             //przekierowanie
         }, function error(response) {
             $scope.czerwony = 4;
-            if(response==2){
-                $scope.tekstBledu = "Błędne dane do logowania!";
-            }
-            else {
-                $scope.tekstBledu = "Błąd podczas połączenia z serwerem";
-            }
+            $scope.tekstBledu = "Błąd podczas połączenia z serwerem";
         });
     };
+
+    //Do Rejestracji
     $scope.zarejestruj = function () {
         $scope.bladHasla = false;
         $scope.bladLoginu = false;
-        if($scope.login+''.length < 5) {
+        $scope.bladEmail = false;
+        if((''+$scope.loginRejestracja).length < 5 || $scope.loginRejestracja === undefined) {
             $scope.bladLoginu = true;
             $scope.trescBladLoginu = "Podałeś zbyt krótki login";
         }
-        if($scope.haslo1+''.length < 7 ){
+        if((''+$scope.haslo1).length < 7 || $scope.haslo1 === undefined){
             $scope.bladHasla = true;
             $scope.trescBladHasla = "Zbyt krótkie hasło!";
-            return;
         }
-        if($scope.haslo1 != $scope.haslo2){
+        else if($scope.haslo1 != $scope.haslo2){
             $scope.bladHasla = true;
             $scope.trescBladHasla = "Podane hasła nie są takie same!";
-            return;
         }
-        if($scope.bladLoginu == true)
+        if ($scope.email === undefined){
+            $scope.bladEmail = true;
+            $scope.trescBladEmail = "Do rejestracji potrzebny jest twój adres E-mail!";
+        }
+        $scope.bladPliku = false;
+        if ($('#zdjecie')[0].files[0].size > 3000000){
+            $scope.bladPliku = true;
+            $scope.trescBladPliku = "Plik jest zbyt duży!";
+        }
+        $scope.bladPliku2 = false;
+        if ($('#CV')[0].files[0].size > 3000000){
+            $scope.bladPliku2 = true;
+            $scope.trescBladPliku2 = "Plik jest zbyt duży!";
+        }
+
+        if($scope.bladLoginu || $scope.bladPliku || $scope.bladPliku2 || $scope.bladHasla || $scope.bladEmail)
             return;
+
+        var rejestracjaW = {
+            login: $socpe.loginRejestracja,
+            haslo: $scope.haslo1,
+            email: $scope.email,
+            imie: $scope.imie,
+            nazwisko: $scope.nazwisko,
+            telefon: $scope.telefon,
+            adres: $scope.adres,
+            minimalne: $scope.minimalne,
+            wyksztalcenie: $scope.wyksztalcenie,
+            doswiadczenie: $scope.doswiadczenie,
+            osiagniecia: $scope.osiagniecia,
+            rodzaj: $scope.zlecenie + $scope.stala + $scope.praktyki,
+            data: $scope.data.toString(),
+            
+        }
         $http({
             method : "POST",
             url : "gooddfsfs.com"
